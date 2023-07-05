8faffbd051e9104da619d075b66782023076f8d2
2445968904 <2445968904@qq.com>
2023-07-05 09:01:05 +0800

    FirstInitailize

---
+++ Source/SimpleEditor/SimpleEditorGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "SimpleEditorGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SIMPLEEDITOR_API ASimpleEditorGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
